dbcaabd3050c96705e222886e52de2565dc72a65
alssel2525 <alssel2525@naver.com>
2022-05-02 07:35:38 +0900

    header 반응형 작성

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
-import profile from "../../images/프사.svg";
 import MediaQuery from "../../hooks/MediaQuery";
+import profile from "../../images/프사.svg";
+import menu from "../../images/menu.svg";
+import close from "../../images/close_FILL0_wght700_GRAD0_opsz40.svg";
+import {useState} from "react";
 
 
 const Container = styled.header`
@@ -15,68 +18,120 @@ const Container = styled.header`
 	left: 0;
 	z-index: 1;
 	background: #ffffff;
+	
+	> .container-1580 {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
+		align-items: center;
+		
+		> .profile {
+			width: 50px;
+			cursor: pointer;
+		}
+		
+		> ul {
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			align-items: center;
+			transition: 0.3s;
 
-	${`@media ${MediaQuery(2)}, ${MediaQuery(3)}`} {
+			> li {
+				position: relative;
+				color: #777777;
+				font-weight: 800;
+				cursor: pointer;
+				transition: 0.3s;
+				
+				:not(:first-of-type) {
+					margin-left: 30px;
+				}
 
+				::after {
+					content: "";
+					width: 0;
+					height: 5px;
+					position: absolute;
+					bottom: -5px;
+					left: calc((100% - 50px) / 2);
+					margin: 0 auto;
+					background: #e5e525;
+					transition: 0.3s;
+				}
+
+				:hover {
+					color: #333333;
+
+					::after {
+						width: 50px;
+					}
+				}
+			}
+		}
+		
+		> .hamburger {
+			display: none;
+			float: right;
+			cursor: pointer;
+		}
+	}
+
+	${`@media ${MediaQuery(3)}, ${MediaQuery(2)}`} {
+		height: 70px;
+		
+		> .container-1580 {
+			width: 100%;
+			height: 100%;
+			padding: 0 40px;
+			display: flex;
+			flex-direction: row;
+			justify-content: space-between;
+			position: relative;
+			background: #ffffff;
+			
+			> ul {
+				width: 100vw;
+				height: 40px;
+				position: fixed;
+				top: 30px;
+				left: 0;
+				background: #eeeeee;
+				opacity: 0;
+				pointer-events: none;
+				z-index: -1;
+				
+				&.active {
+					top: 70px;
+					opacity: 1;
+					pointer-events: auto;
+				}
+			}
+			
+			> .hamburger {
+				display: block;
+				float: right;
+			}
+		}
 	}
 	
 	${`@media ${MediaQuery(1)}`} {
-		> .container-1500 {
+		> .container-1580 {
 			width: 100vw;
 			max-width: 1580px;
 			margin: 0 auto;
 			padding-left: 40px;
-			display: flex;
-			flex-direction: row;
-			justify-content: flex-start;
-			align-items: center;
-
-			> img {
-				margin-right: 20px;
-				height: 50px;
-				transform-origin: right bottom;
-				cursor: pointer;
-			}
+			
 			> ul {
-				display: flex;
-				flex-direction: row;
-				justify-content: flex-start;
-
-				> li {
-					margin-left: 30px;
-					position: relative;
-					color: #777777;
-					font-family: "GmarketSans", sans-serif;
-					font-weight: 800;
-					cursor: pointer;
-					transition: 0.3s;
-
-					::after {
-						content: "";
-						width: 0;
-						height: 5px;
-						position: absolute;
-						bottom: -5px;
-						left: calc((100% - 50px) / 2);
-						margin: 0 auto;
-						background: #e5e525;
-						transition: 0.3s;
-					}
-
-					:hover {
-						color: #333333;
-
-						::after {
-							width: 50px;
-						}
-					}
-				}
+				margin-left: 60px;
 			}
 		}
 	}
 `;
 
 const Header = (props) => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	
 	const ScrollToSection = (index) => {
 		window.scrollTo({
 			top: props.refs.current[index].current.getBoundingClientRect().top + window.scrollY - 110,
@@ -91,12 +146,15 @@ const Header = (props) => {
 		})
 	}
 	
+	const toggleMenu = () => {
+		setIsMenuOpen(prevState => !prevState);
+	}
 	
 	return (
 		<Container>
-			<div className={"container-1500"}>
-				<img src={profile} alt={"방민"} onClick={scrollToTop}/>
-				<ul>
+			<div className={"container-1580"}>
+				<img src={profile} alt={"방민"} className={"profile"} onClick={scrollToTop}/>
+				<ul className={isMenuOpen ? "active" : ""}>
 					<li onClick={() => ScrollToSection(0)}>
 						About
 					</li>
@@ -107,6 +165,7 @@ const Header = (props) => {
 						Contact
 					</li>
 				</ul>
+				<img src={isMenuOpen ? close : menu} alt={"메뉴"} className={"hamburger"} onClick={toggleMenu}/>
 			</div>
 		</Container>
 	)
