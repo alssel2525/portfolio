52e745e16b258eb8ed628df1ab56270cdc0d812c
alssel2525 <alssel2525@naver.com>
2022-05-01 19:58:18 +0900

    media query 작성

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,120 +5,74 @@ import MediaQuery from "../../hooks/MediaQuery";
 
 const Container = styled.header`
 	width: 100vw;
-	display: flex;
-	z-index: 1;
-	
-	${`@media ${MediaQuery(1)}`} {
-		height: 110px;
-		padding-left: 170px;
-		flex-direction: row;
-		justify-content: flex-start;
-		align-items: center;
-		position: fixed;
-		top: 0;
-		left: 0;
-		background: #ffffff;
-		
-		> img {
-			margin-right: 20px;
-			height: 50px;
-			transform-origin: right bottom;
-			cursor: pointer;
-		}
-		
-		> ul {
-			display: flex;
-			flex-direction: row;
-			justify-content: flex-start;
-
-			> li {
-				margin-left: 30px;
-				position: relative;
-				color: #777777;
-				font-family: "GmarketSans", sans-serif;
-				font-weight: 800;
-				cursor: pointer;
-				transition: 0.3s;
-
-				::after {
-					content: "";
-					width: 0;
-					height: 5px;
-					position: absolute;
-					bottom: -5px;
-					left: calc((100% - 50px) / 2);
-					margin: 0 auto;
-					background: #e5e525;
-					transition: 0.3s;
-				}
-
-				:hover {
-					color: #333333;
-
-					::after {
-						width: 50px;
-					}
-				}
-			}
-		}
-	}
-
-
 	height: 110px;
-	padding-left: 170px;
+	display: flex;
 	flex-direction: row;
 	justify-content: flex-start;
 	align-items: center;
 	position: fixed;
 	top: 0;
 	left: 0;
+	z-index: 1;
 	background: #ffffff;
 
-	> img {
-		margin-right: 20px;
-		height: 50px;
-		transform-origin: right bottom;
-		cursor: pointer;
+	${`@media ${MediaQuery(2)}, ${MediaQuery(3)}`} {
+
 	}
+	
+	${`@media ${MediaQuery(1)}`} {
+		> .container-1500 {
+			width: 100vw;
+			max-width: 1580px;
+			margin: 0 auto;
+			padding-left: 40px;
+			display: flex;
+			flex-direction: row;
+			justify-content: flex-start;
+			align-items: center;
 
-	> ul {
-		display: flex;
-		flex-direction: row;
-		justify-content: flex-start;
-
-		> li {
-			margin-left: 30px;
-			position: relative;
-			color: #777777;
-			font-family: "GmarketSans", sans-serif;
-			font-weight: 800;
-			cursor: pointer;
-			transition: 0.3s;
-
-			::after {
-				content: "";
-				width: 0;
-				height: 5px;
-				position: absolute;
-				bottom: -5px;
-				left: calc((100% - 50px) / 2);
-				margin: 0 auto;
-				background: #e5e525;
-				transition: 0.3s;
+			> img {
+				margin-right: 20px;
+				height: 50px;
+				transform-origin: right bottom;
+				cursor: pointer;
 			}
+			> ul {
+				display: flex;
+				flex-direction: row;
+				justify-content: flex-start;
 
-			:hover {
-				color: #333333;
+				> li {
+					margin-left: 30px;
+					position: relative;
+					color: #777777;
+					font-family: "GmarketSans", sans-serif;
+					font-weight: 800;
+					cursor: pointer;
+					transition: 0.3s;
 
-				::after {
-					width: 50px;
+					::after {
+						content: "";
+						width: 0;
+						height: 5px;
+						position: absolute;
+						bottom: -5px;
+						left: calc((100% - 50px) / 2);
+						margin: 0 auto;
+						background: #e5e525;
+						transition: 0.3s;
+					}
+
+					:hover {
+						color: #333333;
+
+						::after {
+							width: 50px;
+						}
+					}
 				}
 			}
 		}
-	}
-
-	${`@media ${MediaQuery(2)}, ${MediaQuery(3)}`} {
-	
 	}
 `;
 
@@ -140,18 +94,20 @@ const Header = (props) => {
 	
 	return (
 		<Container>
-			<img src={profile} alt={"방민"} onClick={scrollToTop}/>
-			<ul>
-				<li onClick={() => ScrollToSection(0)}>
-					About
-				</li>
-				<li onClick={() => ScrollToSection(1)}>
-					Projects
-				</li>
-				<li onClick={() => ScrollToSection(2)}>
-					Contact
-				</li>
-			</ul>
+			<div className={"container-1500"}>
+				<img src={profile} alt={"방민"} onClick={scrollToTop}/>
+				<ul>
+					<li onClick={() => ScrollToSection(0)}>
+						About
+					</li>
+					<li onClick={() => ScrollToSection(1)}>
+						Projects
+					</li>
+					<li onClick={() => ScrollToSection(2)}>
+						Contact
+					</li>
+				</ul>
+			</div>
 		</Container>
 	)
 };
