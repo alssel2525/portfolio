892940abc058ed7af4b02972f2ac84e081de9eef
alssel2525 <alssel2525@naver.com>
2022-05-05 23:13:22 +0900

    Project 영역 media query 작성

--- src/components/Main/Projects/Project/index.js
+++ src/components/Main/Projects/Project/index.js
@@ -1,8 +1,10 @@
 import styled, {css} from "styled-components";
 import useIntersectionObserver from "../../../../hooks/useIntersectionObserver";
+import useMediaQuery from "../../../../hooks/useMediaQuery";
+import MediaQuery from "../../../../hooks/MediaQuery";
 
 const Container = styled.div`
-	width: 1364px;
+	width: 100%;
 	height: 450px;
 	margin: 110px auto;
 	display: flex;
@@ -10,8 +12,9 @@ const Container = styled.div`
 	justify-content: left;
 	align-items: flex-start;
 	position: relative;
-	transition: 0.3s;
+	box-sizing: border-box;
 	
+	transition: 0.3s opacity, 0.3s transform;
 	opacity: 0;
 	transform: translateY(20px);
 	
@@ -21,9 +24,9 @@ const Container = styled.div`
 	`}
 	
 	> img {
-		width: 812px;
+		width: 60%;
 		height: 450px;
-		object-fit: cover;
+		object-fit: contain;
 		box-shadow: #333333 0 0 10px;
 	}
 
@@ -64,16 +67,78 @@ const Container = styled.div`
 			}
 		}
 	}
+	
+	${`@media ${MediaQuery(3)}, ${MediaQuery(2)}`} {
+		width: 100%;
+		height: max-content;
+		flex-direction: column;
+		margin: unset;
+		
+		> img {
+			width: 100%;
+			height: auto;
+		}
+
+		> .content {
+			height: max-content;
+			margin: 30px 0 0;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+			position: relative;
+
+			> .title {
+				font-size: 20px;
+			}
+
+			> .subtitle {
+				margin-top: 20px;
+				font-size: 18px;
+			}
+
+			> .description {
+				margin-top: 30px;
+				line-height: 1.2;
+				font-size: 16px;
+			}
+
+			> .buttons {
+				margin-top: 30px;
+				display: flex;
+
+				> :not(:first-child) {
+					margin-left: 30px;
+				}
+			}
+		}
+	}
+	
+	${`@media ${MediaQuery(2)}`} {
+		> .content {
+			> .title {
+				font-size: 30px;
+			}
+
+			> .subtitle {
+				font-size: 24px;
+			}
+
+			> .description {
+				font-size: 20px;
+			}
+		}
+	}
 `;
 
 const Project = (props) => {
+	const isPC = useMediaQuery(MediaQuery(1));
 	const [ref, isIntersecting] = useIntersectionObserver({
 		threshold: 0.5,
 		rootMargin: "100% 0px 0px 0px",
 	});
 	
 	return (
-		<Container ref={ref} isIntersecting={isIntersecting}>
+		<Container ref={ref} isIntersecting={isPC ? isIntersecting : true} isPC={isPC}>
 			{props.children}
 		</Container>
 	)
--- src/components/Main/Projects/index.js
+++ src/components/Main/Projects/index.js
@@ -4,42 +4,66 @@ import PageButton from "./Project/PageButton";
 import TranslatingImage from "./Project/TranslatingImage";
 import djbm from "../../../images/djbm.jpg";
 import portfolio from "../../../images/portfolio.jpg";
+import styled, {css} from "styled-components";
+import useMediaQuery from "../../../hooks/useMediaQuery";
+import MediaQuery from "../../../hooks/MediaQuery";
 
+
+const ProjectContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+
+	${props => {
+		return !props.isPC && css`
+			> *:not(:first-child) {
+				margin-top: 120px;
+			}
+		`
+	}}
+`
 
 const Projects = (props) => {
+	const isPC = useMediaQuery(MediaQuery(1));
+	
 	return (
 		<Section title={"Projects"} refs={props.refs}>
-			<Project>
-				<TranslatingImage src={djbm} alt={"대전 바이오메디컬 규제자유특구 클론 코딩"}/>
-				<div className={"content"}>
-					<div className={"title"}>대전 바이오메디컬 규제자유특구 홈페이지 클론 코딩</div>
-					<div className={"subtitle"}>기존 사이트를 React로 구현하였습니다.</div>
-					<div className={"description"}>
-						React를 이용하여 SPA 구현<br/>
-						styled-components로 컴포넌트별 디자인<br/>
-						IntersectionObserver를 커스텀 hook으로 활용<br/>
-						Github Pages로 배포</div>
-					<div className={"buttons"}>
-						<PageButton link={"https://alssel2525.github.io/djbm_clone/"}>Link</PageButton>
-						<PageButton link={"https://github.com/alssel2525/djbm_clone"} type={"github"}>Github</PageButton>
+			<ProjectContainer isPC={isPC}>
+				<Project>
+					<TranslatingImage src={djbm} alt={"대전 바이오메디컬 규제자유특구 클론 코딩"}/>
+					<div className={"content"}>
+						<div className={"title"}>대전 바이오메디컬 규제자유특구 홈페이지 클론 코딩</div>
+						<div className={"subtitle"}>기존 사이트를 React로 구현하였습니다.</div>
+						<div className={"description"}>
+							React를 이용하여 SPA 구현<br/>
+							styled-components로 컴포넌트별 디자인<br/>
+							IntersectionObserver를 커스텀 hook으로 활용<br/>
+							Github Pages로 배포
+						</div>
+						<div className={"buttons"}>
+							<PageButton link={"https://alssel2525.github.io/djbm_clone/"}>Link</PageButton>
+							<PageButton link={"https://github.com/alssel2525/djbm_clone"}
+								type={"github"}>Github</PageButton>
+						</div>
 					</div>
-				</div>
-			</Project>
-			<Project>
-				<TranslatingImage src={portfolio} alt={"포트폴리오"}/>
-				<div className={"content"}>
-					<div className={"title"}>포트폴리오</div>
-					<div className={"subtitle"}>개인 포트폴리오 사이트입니다.</div>
-					<div className={"description"}>
-						styled-components로 컴포넌트별 디자인<br/>
-						커서 위치에 따른 이미지 애니메이션 구현<br/>
-						Github Pages로 배포</div>
-					<div className={"buttons"}>
-						<PageButton link={"https://alssel2525.github.io/djbm_clone/"}>Link</PageButton>
-						<PageButton link={"https://github.com/alssel2525/djbm_clone"} type={"github"}>Github</PageButton>
+				</Project>
+				<Project>
+					<TranslatingImage src={portfolio} alt={"포트폴리오"}/>
+					<div className={"content"}>
+						<div className={"title"}>포트폴리오</div>
+						<div className={"subtitle"}>개인 포트폴리오 사이트입니다.</div>
+						<div className={"description"}>
+							styled-components로 컴포넌트별 디자인<br/>
+							커서 위치에 따른 이미지 애니메이션 구현<br/>
+							Github Pages로 배포
+						</div>
+						<div className={"buttons"}>
+							<PageButton link={"https://alssel2525.github.io/djbm_clone/"}>Link</PageButton>
+							<PageButton link={"https://github.com/alssel2525/djbm_clone"}
+								type={"github"}>Github</PageButton>
+						</div>
 					</div>
-				</div>
-			</Project>
+				</Project>
+			</ProjectContainer>
 		</Section>
 	)
 };
